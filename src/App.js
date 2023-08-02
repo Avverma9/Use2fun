b8c3151824b38ad59aea060bb21ef0a84cdbcc29
Abdul-rub <arub0419@gmail.com>
2023-08-02 19:14:12 +0530

    SubAdminAdded

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import AddAdmin from "./Components/Admin/AddAdmin";
 import ManageAdmin from "./Components/Admin/ManageAdmin";
+import AddSubAdmin from "./Components/Admin/SubAdmin/AddSubAdmin";
 import AddAgency from "./Components/Agency/AddAgency";
 import ViewAgency from "./Components/Agency/ViewAgency";
+import ManageSubAdmin from "./Components/Admin/SubAdmin/ManageSubAdmin"
+
 
 function App() {
   return (
     <div className="app">
       {/* <AddAgency/> */}
       {/* <ViewAgency/> */}
-      <ManageAdmin/>
+      {/* <ManageAdmin/> */}
       {/* <AddAdmin/> */}
+      {/* <AddSubAdmin/> */}
+      <ManageSubAdmin/>
     </div>
   );
 }
