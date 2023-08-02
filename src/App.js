8cbf59babf1e1d01271e38a162e5ad0d3b0dbb44
Sourav-source <st.sourav.xaverian@gmail.com>
2023-08-02 21:47:29 +0530

    update 5 files

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Dashboard from "./Components/Dashboard/Dashboard";
 import Header from "./Components/Header/Header";
-
+import Mannageuser from "./Components/Mannageuser/Mannageuser";
+import Sidebar from "./Components/Sidebar/Sidebar";
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <div
-        className="app_body p-5"
-        // contain routes and side navigation
-      >
-        {/* <Dashboard /> */}
-        
-      </div>
-    </Router>
+    <div className="app">
+      <Router>
+        <Header />
+        <Sidebar />
+        <div
+          className="app_body p-5"
+          // contain routes and side navigation
+        >
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/user" element={<Mannageuser />} />
+            <Route path="/view-user" element={<>View</>} />
+          </Routes>
+        </div>
+      </Router>
+    </div>
   );
 }
 
--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,18 +1,24 @@
-import React,{useState}from "react";
-import "./Sidebar.css"
+import { Link } from "react-router-dom";
+import "./Sidebar.css";
 
-const Sidebar=()=>{
-    const [selected,setSelected]=useState("User")
+const Sidebar = () => {
+  // const [selected,setSelected]=useState("User")
 
-    return(
-        <div className="container-sidebar">
-            <div className="sidebar">
-<ul>
-    <li onClick={()=>setSelected("User")}>User</li>
-</ul>
-            </div>
-            {selected === "User" ? (<User/>):null}
-        </div>
-    )
-}
-export default Sidebar
+  return (
+    <div className="container-sidebar">
+      <div className="side_nav_header">
+        <h5>Dashboard</h5>
+      </div>
+      <div className="side_nav_body">
+        <ul>
+          <li>
+            <Link to="/">
+              <p>dashboard</p>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+export default Sidebar;
