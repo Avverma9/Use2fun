df68a944afae7328552a20ef171fc8bfac6c02a1
Abdul-rub <arub0419@gmail.com>
2023-08-11 21:31:54 +0530

    banner-Svip-vip

--- src/Components/Banner/ViewBanner.jsx
+++ src/Components/Banner/ViewBanner.jsx
@@ -1,27 +1,64 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function ViewBanner() {
+  const [bannerData, setBannerData] = useState([]);
+
+  useEffect(() => {
+    // Fetch data from the API endpoint
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://use2fun.onrender.com/admin/banner/getall");
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const jsonData = await response.json();
+        setBannerData(jsonData.data);
+        console.log("Fetched Banner Data:", jsonData.data);
+      } catch (error) {
+        console.error("Error fetching banner data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="main">
       <h3>View Banner</h3>
       <table>
-        <tr>
-          <th>Sr.</th>
-          <th >Banner Image</th>
-          <th width="10px">HyperLink</th>
-          <th>Action</th>
-        </tr>
-        <tr>
-          <td className="bannertd"><div className="num">1</div></td>
-          <td  className="bannertd" ><div className="image"></div></td>
-          <td  className="bannertd"></td>
-          <td  className="bannertd">
-            <select name="cars" id="cars">
-              <option value="Action">Action</option>
-              <option value="saab">Reaction</option>
-            </select>
-          </td>
-        </tr>
+        <thead>
+          <tr>
+            <th>Sr.</th>
+            <th>Banner Image</th>
+            <th>HyperLink</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {bannerData.map((item, index) => (
+            <tr key={item._id}>
+              <td className="bannertd">
+                <div className="num">{index + 1}</div>
+              </td>
+              <td className="bannertd">
+                <div className="image">
+                  <img src={item.img_url} alt="banner" />
+                </div>
+              </td>
+              <td className="bannertd">
+                <a href={item.link} target="_blank" rel="noopener noreferrer">
+                  {item.link}
+                </a>
+              </td>
+              <td className="bannertd">
+                <select name={`action-${item._id}`} id={`action-${item._id}`}>
+                  <option value="Action">Action</option>
+                  <option value="Reaction">Reaction</option>
+                </select>
+              </td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   );
--- src/Components/Svip/AddSvip.jsx
+++ src/Components/Svip/AddSvip.jsx
@@ -1,39 +1,106 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 function AddSvip() {
+  const [formData, setFormData] = useState({
+    svip: null,
+    thumbnail: null,
+    level: '',
+    price: '',
+    day: '',
+
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleFileChange = (e) => {
+    const { name, files } = e.target;
+    if (name === 'svip' || name === 'thumbnail') {
+      if (files.length > 0) {
+        setFormData((prevData) => ({
+          ...prevData,
+          [name]: files[0],
+        }));
+      }
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const formDataToSend = new FormData();
+
+    formDataToSend.append('svip', formData.svip);
+    formDataToSend.append('thumbnail', formData.thumbnail);
+
+    formDataToSend.append('level', formData.level);
+    formDataToSend.append('price', formData.price);
+    formDataToSend.append('day', formData.day);
+
+
+    try {
+      const response = await fetch('https://use2fun.onrender.com/admin/svip/add', {
+        method: 'POST',
+        body: formDataToSend,
+      });
+
+      if (response.ok) {
+        const responseData = await response.json();
+        console.log('Response data:', responseData);
+        toast.success('Agency added successfully.');
+      } else {
+        console.error('Failed to add agency. Response status:', response.status);
+        toast.error('Error occured while adding adding Agent.');
+      }
+    } catch (error) {
+      console.error('Error adding agency:', error);
+    }
+  };
+
+
+
+
   return (
-    <div  className="Vip_main">
-        <h3>Add  Svip</h3>
+    <form className="Vip_main" onSubmit={handleSubmit}>
+      <h3>Add  Svip</h3>
       <div className="innerdiv">
         <label htmlFor="">Image*</label> <br />
         <div className="input">
-          <input type="file" name="" id="" />
+          <input type="file" name="svip" id="" onChange={handleFileChange} />
         </div>
       </div>
       <div className="innerdiv">
         <label htmlFor="">Thumbnail* (MP4)</label>
         <br />
         <div className="input">
-          <input type="file" name="" id="" />
+          <input type="file" name="thumbnail" id="" onChange={handleFileChange}/>
         </div>
       </div>
       <div className="innerdiv">
         <label htmlFor="">Level*</label> <br />
-        <input className="input" type="text" name="" id="" placeholder="Level" />
+        <input className="input" type="number" name="level" id="" placeholder="Level" onChange={handleInputChange} />
       </div>
       <div className="innerdiv">
         <label htmlFor="">Price*</label> <br />
-        <input className="input" type="text" name="" id="" placeholder="Price" />
+        <input className="input" type="number" name="price" id="" placeholder="Price" onChange={handleInputChange} />
       </div>
       <div className="innerdiv">
         <label htmlFor="">Validity* (days)*</label> <br />
-        <input className="input" type="text" name="" id=""  placeholder="Validity"/>
+        <input className="input" type="number" name="" id="" placeholder="Validity" onChange={handleInputChange} />
       </div>
       <div className='Button_div'>
-            <button className='btn btn1'>Cancel</button>
-            <button className='btn btn2'>Submit</button>
-        </div>
-    </div>
+        <button className='btn btn1'>Cancel</button>
+        <button  type="submit" className='btn btn2'>Submit</button>
+      </div>
+    </form>
   )
 }
 
