26f24ee1b27a49a7a47a7a912c7297c825c10bc0
Zishanmohamma <136809335+Zishanmohamma@users.noreply.github.com>
2023-08-12 15:21:44 +0530

    new changes

--- src/Components/Vehicle/ViewVehicle.jsx
+++ src/Components/Vehicle/ViewVehicle.jsx
@@ -1,26 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import React, { useEffect, useState } from 'react';
 
-const ViewFrames = () => {
-  const [frame,setFrame] = useState(null);
-    useEffect(()=>{
-      
-      const fetchData =async ()=>{
-        try{
-          const response = await fetch("https://use2fun.onrender.com/admin/frame/getall");
-          if(!response.ok){
-            throw new Error("network issue");
-          }
-          const jsonData= await response.json();
-          setFrame(jsonData.data);
-          console.log("frame", jsonData.data)
-        } catch (error){
-          console.error("error fetching data" ,error)
-        }
-      }
-      fetchData();
-    },[]);
-const ViewFrames = () => {
+const ViewVehicle = () => {
   const [frame,setFrame] = useState(null);
     useEffect(()=>{
       
@@ -46,15 +26,12 @@ const ViewFrames = () => {
     <div className='viewframe-main'>
         <h3>View Frames</h3>
         <button className='add-frame-btn'>Add Frame</button>
-        <h3>View Frames</h3>
-        <button className='add-frame-btn'>Add Frame</button>
 
 
         <table className="table">
         <thead>
           <tr>
             <th>Sr.</th>
-            <th> Frame Image</th>
             <th> Frame Image</th>
             <th>Price</th>
             <th>Level</th>
@@ -63,16 +40,6 @@ const ViewFrames = () => {
           </tr>
           {frame&&frame.map((item,index)=>(
             <tr key={index}>
-            <td>{index+1}</td>
-            <td><img src={item.img_url} alt='image'/></td>
-            <td>{item.price}</td>
-            <td>{item.level}</td>
-            <td>{item.day}</td>
-            <td>{<select className='viewframe-select'>
-                <option value="action">Action</option>
-                </select>}</td>
-            
-
             <td>{index+1}</td>
             <td><img src={item.img_url} alt='image'/></td>
             <td>{item.price}</td>
@@ -97,4 +64,4 @@ const ViewFrames = () => {
   )
 }
 
-export default ViewFrames
+export default ViewVehicle
