53a66a974a6802902f961088ace57959c316ce7c
Zishanmohamma <136809335+Zishanmohamma@users.noreply.github.com>
2023-08-12 15:22:02 +0530

    new changes

--- src/Components/Svip/ViewSvip.jsx
+++ src/Components/Svip/ViewSvip.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import './ViewSvip.css';
+import { useNavigate } from 'react-router-dom';
 
 function ViewSvip() {
+  const navigate = useNavigate();
   const [viewsvip,setViewsvip] = useState(null);
   useEffect(()=>{
     const fetchData = async()=>{
@@ -21,32 +23,38 @@ function ViewSvip() {
     }
     fetchData();
   },[])
+  const navigatetosvip=()=>{
+    navigate('/add-svip');
+  }
   return (
     <div>
         <div id="vip_main">
       <div className="vip_header">
         <h3>View Svip</h3>
-        <button className="vip_btn">Add Svip</button>
+        <button className="vip_btn" onClick={navigatetosvip}>Add Svip</button>
       </div>
 
       <table id="vip_table">
         <tr>
-          <th className="priceth">Sr.</th>
-          <th className="priceth">Vip Image</th>
-          <th className="priceth" width="10px">
+          <th className="priceth1">Sr.</th>
+          <th className="priceth1">Vip Image</th>
+          <th className="priceth1" width="10px">
             Price
           </th>
-          <th className="priceth">Level</th>
-          <th className="priceth">Validity</th>
-          <th className="priceth">Action</th>
+          <th className="priceth1">Level</th>
+          <th className="priceth1">Validity</th>
+          <th className="priceth1">Action</th>
         </tr>
         {viewsvip&&viewsvip.map((item,index)=>(
           <tr>
-            <td className='priceth'>{index+1}</td>
-            <td className='priceth'><img src='{item.img_url}'/></td>
-            <td className='priceth'>{item.price}</td>
-            <td className='priceth'>{item.level}</td>
-            <td className='priceth'>{item.day}<span>days</span></td>
+            <td className='priceth1'>{index+1}</td>
+            <td className='priceth1'><img src='{item.img_url}'/></td>
+            <td className='priceth1'>{item.price}</td>
+            <td className='priceth1'>{item.level}</td>
+            <td className='priceth1'>{item.day}<span>days</span></td>
+            <td>{<select className='viewsvip-select'>
+                <option value="action">Action</option>
+                </select>}</td>
             
           </tr>
 
