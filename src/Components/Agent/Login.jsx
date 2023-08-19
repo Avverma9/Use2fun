25f912cac04a3dbb861a7d01d561441f44dd5024
Abdul-rub <arub0419@gmail.com>
2023-08-19 12:58:38 +0530

    login-agent

--- src/Components/Agent/Login.jsx
+++ src/Components/Agent/Login.jsx
@@ -1,99 +1,48 @@
-// import React, { useState } from "react";
-// import axios from "axios";
-// import Select from "react-select";
-
-// const AgentLogin = () => {
-//   const [number, setNumber] = useState("");
-//   const [selectedCountry, setSelectedCountry] = useState(null);
-//   const countries = [
-//     { value: "+1", label: "United States", flag: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.vecteezy.com%2Ffree-vector%2Fus-flag-circle&psig=AOvVaw0mgId7BDKs2o7jy1H0fUWt&ust=1691941969655000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCICI_oy914ADFQAAAAAdAAAAABAZ" },
-//     { value: "+91", label: "India", flag: "https://upload.wikimedia.org/wikipedia/en/thumb/4/41/Flag_of_India.svg/1200px-Flag_of_India.svg.png" },
-//   ];
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-
-//     if (selectedCountry && number) {
-//       const response = await axios.post(
-//         "https://use2fun.onrender.com/agent/loginmobile",
-//         {
-//           number: selectedCountry.value + number, // Add country code to the number
-//         }
-//       );
-
-//       if (response.data) {
-//         alert("Data has been received");
-//       }
-//     }
-//   };
-
-//   return (
-//     <>
-//       <form onSubmit={handleSubmit}>
-//         <div className="login-image">
-//           <img src="https://i.gifer.com/IPNp.gif" alt="" />
-//         </div>
-//         <div className="phone-no">
-//           <div className="input-country">
-//             <Select
-//               options={countries}
-//               value={selectedCountry}
-//               onChange={setSelectedCountry}
-//               placeholder="Select country"
-//               getOptionLabel={(option) => (
-//                 <div>
-//                   <img
-//                     src={option.flag}
-//                     alt={option.label}
-//                     style={{ width: "20px", marginRight: "10px" }}
-//                   />
-//                   {option.label}
-//                 </div>
-//               )}
-//             />
-//           </div>
-//           <div className="input-number">
-//             <input
-//               type="text"
-//               value={number}
-//               placeholder="Enter your number"
-//               onChange={(e) => setNumber(e.target.value)}
-//             />
-//           </div>
-//           <button type="submit" className="agent-next">
-//             Next
-//           </button>
-//         </div>
-//       </form>
-//     </>
-//   );
-// };
-
-// export default AgentLogin;
-
-
 import React, { useState } from "react";
 import axios from "axios";
-import "./Login.css"
+import "./Login.css";
+import { useNavigate } from "react-router-dom";
 
 const AgentLogin = () => {
+  const navigate = useNavigate()
+  const [resposneOtp, setResponseOtp] = useState("");
   const [formData, setFormData] = useState({
-    email: "agent007@gmail.com",
-    password: "*********"
+    email: "",
+    otp: "",
+    showOtpInput: false,
   });
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if (formData.email && formData.password) {
-      const response = await axios.post(
-        "https://use2fun.onrender.com/agent/loginmobile",
-        formData
-      );
+    if (formData.email) {
+      try {
+        const response = await axios.post(
+          "https://use2fun.onrender.com/agent/getotp",
+          { email: formData.email }
+        );
 
-      if (response.data) {
-        alert("Data has been received");
+        if (response.data) {
+          setResponseOtp(response.data.otp);
+          setFormData((prevData) => ({
+            ...prevData,
+            showOtpInput: true,
+          }));
+        }
+      } catch (error) {
+        console.error("API Request Error:", error);
       }
+    }
+  };
+
+  const handleOtpSubmit = (e) => {
+    e.preventDefault();
+
+    if (formData.otp === resposneOtp) {
+      alert("Login Successful");
+      navigate("/approved-host-request")
+    } else {
+      alert("Invalid OTP");
     }
   };
 
@@ -101,40 +50,49 @@ const AgentLogin = () => {
     const { name, value } = e.target;
     setFormData((prevData) => ({
       ...prevData,
-      [name]: value
+      [name]: value,
     }));
   };
 
   return (
     <div className="agent-login-container">
-      <form onSubmit={handleSubmit}>
-        <div className="login-image">
-          <img src="https://i.gifer.com/IPNp.gif" alt="" />
-        </div>
-        <div className="input-container">
-          <input
-            type="email"
-            name="email"
-            value={formData.email}
-            placeholder="Enter your email"
-            onChange={handleInputChange}
-          />
-        </div>
-        <div className="input-container">
-          <input
-            type="password"
-            name="password"
-            value={formData.password}
-            placeholder="Enter your password"
-            onChange={handleInputChange}
-          />
-        </div>
-        <p style={{color:"blue"}}>Forgot Password ?</p>
-        <button type="submit" className="agent-next">
-          Next
-        </button>
-      </form>
-   
+      {formData.showOtpInput ? (
+        <form onSubmit={handleOtpSubmit}>
+          <div className="login-image">
+            <img src="https://i.gifer.com/IPNp.gif" alt="" />
+          </div>
+          <div className="input-container">
+            <input
+              type="text"
+              name="otp"
+              value={formData.otp}
+              placeholder="Enter OTP"
+              onChange={handleInputChange}
+            />
+          </div>
+          <button type="submit" className="agent-next">
+            Verify OTP
+          </button>
+        </form>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <div className="login-image">
+            <img src="https://i.gifer.com/IPNp.gif" alt="" />
+          </div>
+          <div className="input-container">
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              placeholder="Enter your email"
+              onChange={handleInputChange}
+            />
+          </div>
+          <button type="submit" className="agent-next">
+            Send OTP
+          </button>
+        </form>
+      )}
     </div>
   );
 };
